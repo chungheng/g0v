a53c0e5d8794a04f451e76e1a1a447afd2fdbbca
Cameron Chen <CameronChenx@gmail.com>
2014-11-22 22:28:13 -0500

    all income htmls

--- pie_chart/06/sequences.js
+++ pie_chart/06/sequences.js
@@ -44,7 +44,7 @@ var arc = d3.svg.arc()
 
 // Use d3.text and d3.csv.parseRows so that we do not need to have a header
 // row, and can receive the csv as an array of arrays.
-d3.text("../../data/donator/Income_第7屆立法委員補選擬參選人蔣乃辛政治獻金專戶.csv", function(text) {
+d3.text("../../data/donator/Income_第8屆立法委員擬參選人徐耀昌政治獻金專戶.csv", function(text) {
   var csv = d3.csv.parseRows(text);
   var json = buildHierarchy(csv);
   createVisualization(json);
